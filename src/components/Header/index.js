ebb13a947b2a66c4f4a2122da882f25422cd2170
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-02-06 22:52:57 -0300

    Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './style.css';
+
+export default function Header() {
+    return (
+        <div className="container">
+            <h1>facebook</h1>
+
+            <div className="profile">
+                <span>Meu perfil</span>
+                <small>Icone</small>
+            </div>
+        </div>
+    );
+}
